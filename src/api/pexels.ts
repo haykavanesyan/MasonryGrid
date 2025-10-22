29d73a9adc030f0349338bdcd20d8d062a6cc54b
haykavanesyan <haykavanesyan09@gmail.com>
2025-10-23 01:04:27 +0400

    fix url to fetch curated data in initial

--- src/api/pexels.ts
+++ src/api/pexels.ts
@@ -8,9 +8,9 @@ export const fetchPhotos = async (
   page = 1,
   query: string,
 ): Promise<Photo[]> => {
-  const { data } = await axios.get(`${BASE_URL}/search`, {
+  const { data } = await axios.get(`${BASE_URL}/${query ? 'search' : 'curated'}`, {
     headers: { Authorization: API_KEY },
-    params: { query: query || "nature", per_page: 30, page },
+    params: { query, per_page: 30, page },
   });
   return data.photos;
 };
